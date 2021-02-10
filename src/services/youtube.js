3ad856a312b2f16ade36d1f05d8528e32243464d
silviapio <silvia.piovesan@gmail.com>
2021-02-10 16:54:58 +0100

    set api call for mostPopular and related videos

--- src/services/youtube.js
+++ src/services/youtube.js
@@ -1,19 +1,47 @@
 import axios from 'axios';
 import { youtubeKey } from './privateKeys';
 
-export const getSearchResult = searchText =>
-    axios.get('https://www.googleapis.com/youtube/v3/search', {
-        params: {
-            part: "snippet",
-            maxResults: 5,
-            type: "video",
-            q: searchText,
-            key: youtubeKey,
-            videoEmbeddable: true,
-            relevanceLanguage: "en",
-        },
-        headers: {
-            "Accept": "application/json"
-        }
-    })
-;
+export const getYoutubeResult = (searchText, mainVideoId) => {
+    if (!searchText) {
+        if (!mainVideoId) {
+            return apicall("recommended");
+        } else {
+            return apicall("search", mainVideoId);
+        }        
+    } else {
+        return apicall("search", null, searchText);
+    }
+}
+
+const apicall = (callType, videoId, text) => {
+    if (callType === "search") {
+        return axios.get('https://www.googleapis.com/youtube/v3/search', {
+                params: {
+                    part: "snippet",
+                    maxResults: 5,
+                    type: "video",
+                    relatedToVideoId: videoId,
+                    q: text,
+                    key: youtubeKey,
+                    videoEmbeddable: true,
+                    relevanceLanguage: "en",
+                },
+                headers: {
+                    "Accept": "application/json"
+                }
+            });
+    } else if (callType === "recommended") {
+        return axios.get('https://www.googleapis.com/youtube/v3/videos', {
+            params: {
+                part: "snippet",
+                chart: "mostPopular",
+                regionCode: "ES",
+                key: youtubeKey,
+                videoEmbeddable: true
+            },
+            headers: {
+                "Accept": "application/json" 
+            }
+        });
+    }
+};
