e97fdd5f44576895aecd3723b184de203fd4f8e4
Silvia <silvia.piovesan@gmail.com>
2022-02-20 16:14:30 +0100

    change api key

--- src/components/App.js
+++ src/components/App.js
@@ -9,34 +9,36 @@ import { MyGrid, MyRow, MyCol } from "./screens/Home.styles";
 import logoWords from "../images/reactube.svg";
 import logoIcon from "../images/reactube-logo-camera.svg";
 
-const App = () => (
-  <Router>
-    <GlobalStyle />
-    <MyGrid fluid>
-      <MyRow xs={12}>
-        <MyCol xs={12} md={3} lg={2} className="sidebarCol">
-          <SidebarContainer>
-            <Link to="/">
-              <header>
-                <div>
-                  <LogoIcon src={logoIcon} alt="videocamera icon" className="logo__icon" />
-                  <LogoWords src={logoWords} alt="reactube" className="logo__words" />
-                </div>
-              </header>
-            </Link>
-            <Menu />
-          </SidebarContainer>
-        </MyCol>
-        <MyCol xs={12} md={9} lg={10}>
-          <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/videoDetail/:id" component={VideoDetailPage} />
-            <Route path="/favorites" component={Favorites} />
-            <Route path="/history" component={SearchHistory} />
-          </Switch>
-        </MyCol>
-      </MyRow>
-    </MyGrid>
-  </Router>
-);
+function App() {
+  return (
+    <Router>
+      <GlobalStyle />
+      <MyGrid fluid>
+        <MyRow xs={12}>
+          <MyCol xs={12} md={3} lg={2} className="sidebarCol">
+            <SidebarContainer>
+              <Link to="/">
+                <header>
+                  <div>
+                    <LogoIcon src={logoIcon} alt="videocamera icon" className="logo__icon" />
+                    <LogoWords src={logoWords} alt="reactube" className="logo__words" />
+                  </div>
+                </header>
+              </Link>
+              <Menu />
+            </SidebarContainer>
+          </MyCol>
+          <MyCol xs={12} md={9} lg={10}>
+            <Switch>
+              <Route path="/" exact component={Home} />
+              <Route path="/videoDetail/:id" component={VideoDetailPage} />
+              <Route path="/favorites" component={Favorites} />
+              <Route path="/history" component={SearchHistory} />
+            </Switch>
+          </MyCol>
+        </MyRow>
+      </MyGrid>
+    </Router>
+  );
+}
 export default App;
--- src/services/youtube.js
+++ src/services/youtube.js
@@ -1,62 +1,64 @@
-import axios from 'axios';
-import { youtubeKey } from './privateKeys';
+import axios from "axios";
+//import { youtubeKey } from './privateKeys';
+
+const youtubeKey = process.env.REACT_APP_API_KEY;
 
 export const getYoutubeResult = (searchText, mainVideoId) => {
-    if (!searchText) {
-        if (!mainVideoId) {
-            return apicall("videos");
-        } else {
-            return apicall("search", mainVideoId);
-        }        
+  if (!searchText) {
+    if (!mainVideoId) {
+      return apicall("videos");
     } else {
-        return apicall("search", null, searchText);
+      return apicall("search", mainVideoId);
     }
-}
+  } else {
+    return apicall("search", null, searchText);
+  }
+};
 
 export const getSingleVideoInfo = videoId => apicall("videos", videoId);
 
 const apicall = (callType, videoId, text) => {
-    if (callType === "search") {
-        return axios.get('https://www.googleapis.com/youtube/v3/search', {
-                params: {
-                    part: "snippet",
-                    maxResults: 10,
-                    type: "video",
-                    relatedToVideoId: videoId,
-                    q: text,
-                    key: youtubeKey,
-                    videoEmbeddable: true,
-                    relevanceLanguage: "en",
-                },
-                headers: {
-                    "Accept": "application/json"
-                }
-            });
-    } else if (callType === "videos") {
-        if (videoId) {
-            return axios.get('https://www.googleapis.com/youtube/v3/videos', {
-                params: {
-                    part: "snippet",
-                    key: youtubeKey,
-                    id: videoId
-                },
-                headers: {
-                    "Accept": "application/json" 
-                }
-            });
-        } else {
-            return axios.get('https://www.googleapis.com/youtube/v3/videos', {
-                params: {
-                    part: "snippet",
-                    chart: "mostPopular",
-                    regionCode: "ES",
-                    key: youtubeKey,
-                    videoEmbeddable: true
-                },
-                headers: {
-                    "Accept": "application/json" 
-                }
-            });
-        }    
+  if (callType === "search") {
+    return axios.get("https://www.googleapis.com/youtube/v3/search", {
+      params: {
+        part: "snippet",
+        maxResults: 10,
+        type: "video",
+        relatedToVideoId: videoId,
+        q: text,
+        key: youtubeKey,
+        videoEmbeddable: true,
+        relevanceLanguage: "en",
+      },
+      headers: {
+        Accept: "application/json",
+      },
+    });
+  } else if (callType === "videos") {
+    if (videoId) {
+      return axios.get("https://www.googleapis.com/youtube/v3/videos", {
+        params: {
+          part: "snippet",
+          key: youtubeKey,
+          id: videoId,
+        },
+        headers: {
+          Accept: "application/json",
+        },
+      });
+    } else {
+      return axios.get("https://www.googleapis.com/youtube/v3/videos", {
+        params: {
+          part: "snippet",
+          chart: "mostPopular",
+          regionCode: "ES",
+          key: youtubeKey,
+          videoEmbeddable: true,
+        },
+        headers: {
+          Accept: "application/json",
+        },
+      });
     }
+  }
 };
